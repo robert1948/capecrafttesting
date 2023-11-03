b1f5c6eb58afc659fabb28902791d92d5eefdbd1
robert1948 <46155205+robert1948@users.noreply.github.com>
2023-11-03 11:12:56 +0200

    Add files via upload

--- projects/settings.py
+++ projects/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@cnkkngj$v_p@6arhu4kk+3crwcdg$_k&i$(pc&#drsod#-kbg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
