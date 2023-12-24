0532fd47e3578c47e609d45e29dab3b811e09f45
Robert Kleyn <zeonita@gmail.com>
2023-12-24 06:01:47 +0200

    Updated Templates structure

--- projects/settings.py
+++ projects/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'projects.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
