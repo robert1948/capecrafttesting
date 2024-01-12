1f268707e38c63bbc003da1720ccd2e6e51d3276
Robert Kleyn <zeonita@gmail.com>
2024-01-12 10:13:41 +0200

    Fix login and registration forms

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -3,30 +3,34 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-     email = forms.EmailField(required=True)
-     first_name = forms.CharField(max_length=100, required=True)
-     last_name = forms.CharField(max_length=100, required=True)
-    # username = forms.CharField(max_length=100, required=True)
-    # password1 = forms.CharField(widget=forms.PasswordInput)
-    # password2 = forms.CharField(widget=forms.PasswordInput)
-    # 
-    # class Meta:
-    #     model = User
-    #     fields = ('email', 'first_name', 'last_name', 'username', 'password1', 'password2')
-    # 
-    # def save(self, commit=True):
-    #     user = super(SignUpForm, self).save(commit=False)
-    #     user.email = self.cleaned_data['email']
-    #     user.first_name = self.cleaned_data['first_name']
-    #     user.last_name = self.cleaned_data['last_name']
-    #     user.username = self.cleaned_data['username']
-    #     user.password1 = self.cleaned_data['password1']
-    #     user.password2 = self.cleaned_data['password2']
-    # 
-    #     if commit:
-    #         user.save()
-    # 
-    #     return user
-class Meta:
+    email = forms.EmailField(
+        label="",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'})
+    )
+    first_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'})
+    )
+    last_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    )
+
+    class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'username', 'password1', 'password2',)
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<small>Required. 150 characters or fewer. Letters, digits, and @/./+/-/_ only.</small>'
+
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password1'].label = ''
+
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
+        self.fields['password2'].label = ''
